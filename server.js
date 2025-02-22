85c70379891e29f6998840e9b4679f057181cd7a
Jerome-ant <jerome@antares.work>
2025-02-22 23:10:03 +0100

    Correction du port

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Serveur backend lancé sur http://0.0.0.0:${PORT}`);
+});
 
 // 📌 Activer CORS et JSON parsing
 app.use(cors());
